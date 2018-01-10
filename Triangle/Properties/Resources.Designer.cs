3d107dd3829efaacc16fd2328491f2c320b67854
KristelEiTea <kristel.siilak@hotmail.com>
2018-01-10 10:12:03 +0200

    dfghj

--- Triangle/Properties/Resources.Designer.cs
+++ Triangle/Properties/Resources.Designer.cs
@@ -10,6 +10,7 @@
 
 namespace Triangle.Properties
 {
+    using System;
 
 
     /// <summary>
@@ -19,7 +20,7 @@ namespace Triangle.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources
@@ -42,7 +43,7 @@ namespace Triangle.Properties
         {
             get
             {
-                if ((resourceMan == null))
+                if (object.ReferenceEquals(resourceMan, null))
                 {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Triangle.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
@@ -65,6 +66,138 @@ namespace Triangle.Properties
             set
             {
                 resourceCulture = value;
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _16GB
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("16GB", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap JBL_extreme
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("JBL extreme", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap jbl_karp
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("jbl karp", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap KeyboardLED
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("KeyboardLED", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap LG_mon
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("LG mon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap LG_TV
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("LG TV", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap MyPhone
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("MyPhone", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Nimetu
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("Nimetu", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap nööpklapid
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("nööpklapid", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap thOHG8WHF9
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("thOHG8WHF9", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap wir_mous
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("wir mous", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
