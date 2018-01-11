957b27739ffb045359a485c894a971d240236949
KristelEiTea <kristel.siilak@hotmail.com>
2018-01-11 14:08:08 +0200

    etr

--- Triangle/Tehnika.Designer.cs
+++ Triangle/Tehnika.Designer.cs
@@ -54,6 +54,9 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.Algusesse = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
@@ -371,12 +374,45 @@
             this.Algusesse.UseVisualStyleBackColor = false;
             this.Algusesse.Click += new System.EventHandler(this.Algusesse_Click);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(624, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 33);
+            this.button1.TabIndex = 55;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(841, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 33);
+            this.button2.TabIndex = 56;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(624, 134);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 33);
+            this.button3.TabIndex = 57;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Tehnika
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.ClientSize = new System.Drawing.Size(1129, 633);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.Algusesse);
             this.Controls.Add(this.alert);
             this.Controls.Add(this.pictureBox10);
@@ -449,5 +485,8 @@
         private System.Windows.Forms.Button SklapidNupp;
         private System.Windows.Forms.Button NklapidNupp;
         private System.Windows.Forms.Button Algusesse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
